51d9545aa98ca86e8101870b5b974bc12c127954
enzo <enzovcnt@gmail.com>
2024-11-16 00:17:42 +0100

    Try to choose a random word in the table

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 let dictionnaire = ["BONJOUR","VIE","NATURE","MAISON"]
+let alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+
+
+console.log(dictionnaire)
+
+function motRandom (){
+    let choixmot = Math.round(Math.random()*10);
+}
+let choixmot = Math.round(Math.random());
+console.log(choixmot)
