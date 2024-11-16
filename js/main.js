5694f132c223c8fea95be4001f8e469cfd98029d
enzo <enzovcnt@gmail.com>
2024-11-16 17:24:55 +0100

    when click on the button the HTML appears and change.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@ let alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 function motRandom(){
     let idx = Math.floor(Math.random() * dictionnaire.length);
     let dico = dictionnaire[idx];
+    apparition.innerHTML = dico
     console.log(dico);
 }
 
-console.log(motRandom());
+const apparition = document.querySelector("p")
+const btnStart = document.querySelector(".btnStart")
 
-
+btnStart.addEventListener("click", ()=>{
+    motRandom();
+})
